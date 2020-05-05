ff2d2b74312287b46ec9118f8f67a3d3a21e6431
Richard Davis <richard.trow.davis@gmail.com>
2020-05-05 10:07:59 -0700

    Added error message and arrow to input when the input is not valid - Need to work on edge cases for the input so that it doesn't return errors if clicked twice

--- validation.js
+++ validation.js
@@ -1,9 +1,21 @@
-function checkValidation() {
+function checkValidation(event) {
+  event.preventDefault();
   let input = document.getElementsByClassName("description__input")[0];
-  input.style.border = "none";
-  input.classList.add("description__validation");
-  if ([...input.classList].includes("description__validation")) {
+  let errorArrow = document.querySelector(".description__error");
+  let errorMessage = document.querySelector(".description__error-message");
+
+  if (!input.checkValidity()) {
+    input.style.border = "none";
     input.style.border = "2px solid red";
+    input.setCustomValidity("HELLO");
+    console.log(errorArrow);
+    errorArrow.classList.remove("description__error");
+    errorArrow.classList.add("description__error--active");
+    errorMessage.classList.remove("description__error-message");
+    errorMessage.classList.add("description__error-message--active");
   }
-  console.log(input.classList);
+
+  if (input.checkValidity()) {
+    input.style.border = "2px solid turquoise";
+  }
 }
